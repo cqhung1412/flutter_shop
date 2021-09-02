3ba6eea52caf8de371addc50723d59b106f10ecc
cqhung1412 <wokhug99@gmail.com>
2021-09-02 17:09:21 +0700

    using consumer for opt

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,7 +7,10 @@ import '../screens/product_detail_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(5),
       child: GridTile(
@@ -25,12 +28,14 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, prod, child) => IconButton(
+              icon: Icon(
+                prod.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: prod.toggleFavoriteState,
             ),
-            color: Theme.of(context).accentColor,
-            onPressed: product.toggleFavoriteState,
           ),
           title: Text(
             product.name,
