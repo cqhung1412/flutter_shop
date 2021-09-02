72f32f7289d6d0f9a9fb5afb0d146f5d8d7d5559
cqhung1412 <wokhug99@gmail.com>
2021-09-02 20:47:19 +0700

    use multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './styles/my_colors.dart';
+import 'styles/my_colors.dart';
 import 'screens/product_overview_screen.dart';
 import 'screens/product_detail_screen.dart';
-import './providers/products.dart';
+import 'providers/products.dart';
+import 'providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -6,35 +6,35 @@ class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
-      name: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
+      name: 'Arina Hashimoto',
+      description: 'It is pretty red!',
       price: 29.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://gamek.mediacdn.vn/133514250583805952/2020/6/17/photo-1-15924015006991181398792.jpg',
     ),
     Product(
       id: 'p2',
-      name: 'Trousers',
-      description: 'A nice pair of trousers.',
+      name: 'Kana Momonogi',
+      description: 'A nice pair of ...',
       price: 59.99,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://gamek.mediacdn.vn/thumb_w/640/133514250583805952/2020/5/27/-15905513653851678901709.jpg',
     ),
     Product(
       id: 'p3',
-      name: 'Yellow Scarf',
+      name: 'Eimi Fukada',
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://gamek.mediacdn.vn/133514250583805952/2021/6/15/photo-1-16237578241171154845758.jpg',
     ),
     Product(
       id: 'p4',
-      name: 'A Pan',
+      name: 'Yua Mikami',
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://cdn.hit.vn/toquoc/55309ff1-1d3d-4478-4ba6-076f9fd0d28b/5d4b8d81-f3b2-42d2-40b9-a4b2001fb7ba.jpg',
     ),
   ];
 
