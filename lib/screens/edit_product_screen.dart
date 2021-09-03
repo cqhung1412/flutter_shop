ae106619b546ef1f732dbbce7d1f3b5bc3144121
cqhung1412 <wokhug99@gmail.com>
2021-09-03 15:22:07 +0700

    multiline input and dispose focus node

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -15,6 +15,14 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +47,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.number,
                   focusNode: _priceFocusNode,
+                  onFieldSubmitted: (_) =>
+                      Focus.of(context).requestFocus(_descFocusNode),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Description'),
+                  maxLines: 3,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.multiline,
+                  focusNode: _descFocusNode,
                 ),
               ],
             ),
